3261ee9f0cf1869cbb3eb3b881e7bb5058c18a54
Османов Никита <osmanov.nekit@mail.ru>
2021-10-09 15:21:44 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateAuthorize
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBContainer db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new DBContainer();
+        }
+
+        private void AuthorizationClick(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "" || password.Password == "")
+                MessageBox.Show("Ошибка: пустое поле!");
+            else if (db.UserSet.Select(item => item.Login + " " + item.Password).Contains(login.Text + " " + password.Password))
+                MessageBox.Show("Успех: авторизация!");
+            else
+                MessageBox.Show("Ошибка: неверный логин или пароль!");
+        }
+
+        private void RegistrationClick(object sender, RoutedEventArgs e)
+        {
+            new RegistrationWindow().Show();
+            Close();
+        }
+    }
+}
